8097db013dda200ff395cf9058459192dc43a654
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-01-08 12:05:34 +0000

    Add layout

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="p-4">
+        Custom layout defined with <code>definePageMeta</code>
+
+        <NuxtLink to="/">
+            Back to home
+        </NuxtLink>
+    </div>
+</template>
+
+<script>
+definePageMeta({
+    layout: 'default'
+})
+</script>
