fd307333b829c40a1c77f2c7d63c41bcd750a2df
José Manuel Piñeiro García <josempga@ext.inditex.com>
2023-01-20 08:57:48 +0000

    Refactor linkedin banner

---
+++ components/banners/linkedin/socials.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="Socials">
+    <div class="Social">
+      <IconGithub /> <span class="hash">josempineiro</span>
+    </div>
+    <div class="Social">
+      <IconTwitter /> <span class="hash">navylemux</span>
+    </div>
+    <div class="Social">
+      <IconLinkedIn /> <span class="hash">josempineiro</span>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss">
+.Socials {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+  .Social {
+    display: flex;
+    gap: 16px;
+    justify-content: space-evenly;
+    color: #fff;
+  }
+}
+</style>
---
+++ components/banners/linkedin/techs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Techs">
+    <IconJavascript class="Icon" />
+    <IconTypescript class="Icon" />
+    <IconVue class="Icon" />
+    <IconNuxt class="Icon" />
+    <IconReact class="Icon" />
+    <IconNext class="Icon" />
+    <IconCSS class="Icon" />
+    <IconTailwind class="Icon" />
+    <IconSass class="Icon" />
+    <IconGraphQL class="Icon" />
+    <IconApolloGraphQL class="Icon" />
+    <IconNPM class="Icon" />
+    <IconWebpack class="Icon" />
+    <IconRollup class="Icon" />
+  </div>
+</template>
+<script setup></script>
+<style lang="scss">
+.Techs {
+  display: flex;
+  flex-direction: row;
+  gap: 32px;
+  padding: 0;
+  .Icon {
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>
